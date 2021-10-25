7b43fe2a55875a5af1fac1e60be0c32b90060ce2
dmaisson <maiss002@umn.edu>
2021-10-25 16:27:02 -0500

    able to trialize spk and lfp, if the input starts out in the same structure

--- Individual Analyses/Big Cage/Timescales/SpikeTime_trialization.m
+++ Individual Analyses/Big Cage/Timescales/SpikeTime_trialization.m
@@ -1,1 +1,49 @@
-function trials = SpikeTime_trialization(in)
+function psth = SpikeTime_trialization(in,lfp)
+
+% in should be the cell-separated structure array
+    %spikeSeries is the structure field containing the raw spike series
+    %lfp: a binary (yes/no) argument indicating whether the data being
+        %trialized is the processed LFP time series
+    %lfpSeries is the down-sampled, pre-processed LFP time series field
+    
+spikeSeries = in.spikeSeries;
+if lfp == 1
+    lfpSeries = in.lfpSeries;
+end
+spike_idx = find(spikeSeries == 1);
+
+s = size(spikeSeries,2); %data is time binned by capture data frame-rate (30Hz)
+
+window_2seconds = 60; %30 frames is 1 second, so 60 frames (i.e. 60 time bins) is 2 seconds of data
+
+n = size(spike_idx,2); %how many trials there will be, based on how many spikes there were
+
+psth = zeros(n,window_2seconds); %initialize an empty variable
+
+if lfp == 0 %if we're dealing with spikes
+    for iA = 1:n %iterate through the trials
+        if spike_idx(iA)+(window_2seconds-1) < s %if there are enough data points left in the series to fit into a trial
+            psth(iA,:) = spikeSeries(1,spike_idx(iA):spike_idx(iA)+(window_2seconds-1)); %take the spike as the first time point, and add 2 second of the signal to the rest of the trial
+        else %if the spike happened toward the end, and there aren't enough time bins left to complete the trial
+            temp = spikeSeries(1,spike_idx(iA):end); %place holder for the the content that is available
+            append_var = size(temp,2); %determine how many bins are missing to be a complete trial
+            cat_var = zeros(1,size(psth,2) - append_var); %create a vector of zeros for whatever is required to complete the trial
+            temp = cat(2,temp,cat_var); %concatenate the two
+            psth(iA,:) = temp; %add those zeros to the end of the "trial"
+        end
+    end
+else %if we're dealing with the LFP, do the same but store the LFP series values
+    for iA = 1:n
+        if spike_idx(iA)+(window_2seconds-1) < s
+            psth(iA,:) = lfpSeries(1,spike_idx(iA):spike_idx(iA)+(window_2seconds-1));
+        else
+            temp = lfpSeries(1,spike_idx(iA):end);
+            append_var = size(temp,2);
+            cat_var = zeros(1,size(psth,2) - append_var);
+            temp = cat(2,temp,cat_var);
+            psth(iA,:) = temp;
+        end
+    end
+end
+
+end
