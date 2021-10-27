225de8d781fa7e09428dc9694b4a61501749ce20
dmaisson <maiss002@umn.edu>
2021-10-27 15:39:11 -0500

    code fixes

--- Individual Analyses/Big Cage/Timescales/Epoch_trialization.m
+++ Individual Analyses/Big Cage/Timescales/Epoch_trialization.m
@@ -9,43 +9,47 @@ function psth = Epoch_trialization(in,lfp,trial_length,epochs)
         %a start time
     %epochs: indicates the number of continuous epochs
 
-    
-    spikeSeries = in.spikeSeries;
-    if lfp == 1
-        lfpSeries = in.lfpSeries;
-    end
-    
-    s = size(spikeSeries,2); %data is time binned by capture data frame-rate (30Hz)
-    epoch_window = s/epochs;
-    epoch_segments = cell(epochs,1);
-    start = 1;
-
-    if lfp == 0
-        for iA = 1:epochs
-            epoch_segments{iA,1} = spikeSeries(1,start:(start+epoch_window)-1);
-            start = start+epoch_window;
-        end
-    else
-        for iA = 1:epochs
-            epoch_segments{iA,1} = lfpSeries(1,start:(start+epoch_window)-1);
-            start = start+epoch_window;
-        end
-    end
-    
+        
     window_size = trial_length*30; %30 frames is 1 second, so 60 frames (i.e. 60 time bins) is 2 seconds of data
+    s = size(in{1}.spikeSeries,2); %data is time binned by capture data frame-rate (30Hz)
+    epoch_window = s/epochs;
+    n = epoch_window/window_size;
+    epoch_segments = cell(epochs,1);
     
-    n = epoch_window/window_size; %how many trials there will be, based on how many spikes there were
-    
-    for iA = 1:epochs
-        psth{iA,1} = zeros(n,window_size); %initialize an empty variable
+    for iB = 1:epochs
+        for iC = 1:size(in,1)
+            psth{iB,1}{iC,1} = NaN(n,window_size); %initialize an empty variable
+        end
     end
     
-    for iA = 1:epochs
-        start = 1;
-        for iB = 1:n
-            psth{iA,1}(iB,:) = epoch_segments{iA,1}(1,start:(start+window_size)-1);
-            start = start+window_size;
+    for iA = 1:size(in,1)
+        spikeSeries = in{iA}.spikeSeries;
+        if lfp == 1
+            lfpSeries = in{iA}.lfpSeries;
         end
+        
+        start = 1;
+        if lfp == 0
+            for iB = 1:epochs
+                epoch_segments{iB,1} = spikeSeries(1,start:(start+epoch_window)-1);
+                start = start+epoch_window;
+            end
+        else
+            for iB = 1:epochs
+                epoch_segments{iB,1} = lfpSeries(1,start:(start+epoch_window)-1);
+                start = start+epoch_window;
+            end
+        end %how many trials there will be, based on how many spikes there were
+        
+        start = 1;
+        for iB = 1:epochs
+            start = 1;
+            for iC = 1:n
+                psth{iB,1}{iA,1}(iC,:) = epoch_segments{iB,1}(1,start:(start+window_size)-1);
+                start = start+window_size;
+            end
+        end
+    clear epoch_segments start spikeSeries lfpSeries
     end
 
 end
